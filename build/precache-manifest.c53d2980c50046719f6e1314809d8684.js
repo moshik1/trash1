35c0ae25b633dd58f6aac2f8d4673705ca080e7a
Moshik Hershcovitch <moshik1@gmail.com>
2022-12-21 15:55:19 +0200

    Initial commit

---
+++ build/precache-manifest.c53d2980c50046719f6e1314809d8684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb557920cbac141754dd0bdfbb5a1b08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7776acfebb5ce5fc61f6",
+    "url": "/static/css/main.231ddb9c.chunk.css"
+  },
+  {
+    "revision": "39a928e8bf8ffeec3dd6",
+    "url": "/static/js/2.ea681057.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ea681057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7776acfebb5ce5fc61f6",
+    "url": "/static/js/main.4399a2b8.chunk.js"
+  },
+  {
+    "revision": "abfb3ff46297fda5257c",
+    "url": "/static/js/runtime-main.64b9faf1.js"
+  }
+]);
